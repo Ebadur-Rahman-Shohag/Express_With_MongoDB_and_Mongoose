b1dcfce469dce12fd1775b6b885845c3aba44a8c
Ebadur Rahman <ebadurshohag@gmail.com>
2024-05-19 18:08:30 +0600

    add utils folder

--- copy-files/tourController copy3.mjs
+++ copy-files/tourController copy3.mjs
@@ -1,9 +1,9 @@
 import Tour from '../models/tourModels.mjs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import APIFeatures from '../utils/apiFeatures.mjs';
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+import { match } from 'assert';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 //TODO:.Route Handler Function
 
@@ -15,6 +15,59 @@ const aliasTopTours = (req, res, next) => {
   next();
 };
 
+//Refactoring our code
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
 //***tour
 const getAllTours = async (req, res) => {
   try {
